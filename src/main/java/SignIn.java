1c05aa08b80212be1430c37f852726bc985633cd
Сафонова Наталья Владимировна <nvsafonova@phoenixit.ru>
2022-10-26 14:04:29 +0300

    1.0

---
+++ src/main/java/SignIn.java
@@ -0,0 +1,19 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.By;
+
+public class SignIn {
+
+    private WebDriver driver;
+
+    public SignIn(WebDriver driver){
+        this.driver = driver;
+    }
+
+    private By emailInput = By.xpath("//input[@name=\"login[username]\"]");
+    private By passwordInput = By.xpath("//input[@name=\"login[password]\"]");
+    private By loginBtn = By.xpath("//button[@id=\"login-form-btn\"]");
+
+    public void clickSignIn(){
+        driver.findElement(loginBtn).click();
+    }
+}
---
+++ src/main/java/SignUp.java
@@ -0,0 +1,27 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class SignUp {
+    private WebDriver driver;
+
+    public SignUp(WebDriver driver){
+        this.driver = driver;
+    }
+
+    private By nameInput = By.xpath("//input[@id=\"fullname\"]");
+    private By phoneInput = By.xpath("//input[@id=\"rmobile\"]");
+    private By emailInput = By.xpath("//input[@id=\"email\"]");
+    private By passwordInput = By.xpath("//input[@id=\"password\"]");
+    private By confirmationInput = By.xpath("//input[@id=\"confirmation\"]");
+    private By checkBox = By.xpath("//div[contains(@class, \"input-checkbox\")]");
+    private By createBtn = By.xpath("//button[@id=\"create-form-btn\"]");
+
+    public void clickSignUp(){
+        driver.findElement(createBtn).click();
+    }
+
+//    public SignIn clickSignIn(){
+//        driver.findElement(loginBtn).click();
+//        return new SignIn()
+//    }
+}
