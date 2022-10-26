a0fd18de33687eff80b93ac1081a9686aa7530af
Natalia <inatchan@icloud.com>
2022-10-26 21:56:07 +0300

    1.5

--- src/main/java/MainClass.java
+++ src/main/java/MainClass.java
@@ -21,6 +21,7 @@ public class MainClass {
         SignIn signIn = new SignIn(driver);
 
         //mainPage.closeAction();
+        //
         mainPage.openMenu();
         mainPage.clickSignIn();
 
